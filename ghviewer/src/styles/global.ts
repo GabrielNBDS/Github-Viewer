766e7f6fbdcf79fcae482bb7feb96fce4729f9c1
GabrielNBDS <gabriel.nbds@gmail.com>
2020-06-08 15:12:48 -0400

    Modifying global styles

--- ghviewer/src/styles/global.ts
+++ ghviewer/src/styles/global.ts
@@ -11,18 +11,25 @@ export default createGlobalStyle`
   body{
     background: #F0F0F5;
     -webkit-font-smoothing: antialiased;
+    font-weight: 300;
+    color: #333;
   }
 
   body, input, button {
     font-family: 'Roboto', sans-serif;
   }
 
-  button{
+  button, a{
     cursor: pointer;
   }
 
+  a {
+    color: #333;
+    text-decoration: none;
+  }
+
   #root {
-    max-width: 960px;
+    max-width: 800px;
     margin: 0 auto;
     padding: 40px 20px;
   }
