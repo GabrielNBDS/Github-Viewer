bef82adaa82be56b7aecebdc8fc722bbf88a36f1
GabrielNBDS <gabriel.nbds@gmail.com>
2020-06-09 17:14:24 -0400

    Data Shown in Details is now dynamic

--- ghviewer/src/pages/Details/index.tsx
+++ ghviewer/src/pages/Details/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouteMatch } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import Header from '../../components/Header';
 import { UserInfo, Info } from './styles';
+
+import RepoProps from '../../interfaces/RepoProps';
+import UserProps from '../../interfaces/UserProps';
 
 interface RouteParam {
   user: string;
@@ -11,35 +16,76 @@ interface RouteParam {
 const Details: React.FC = () => {
   const { params } = useRouteMatch<RouteParam>();
 
+  const [user, setUser] = useState<UserProps | null>(null);
+  const [repos, setRepos] = useState<RepoProps[]>();
+  const [followers, setFolloweres] = useState<number>();
+  const [following, setFollowing] = useState<number>();
+
+  useEffect(() => {
+    api
+      .get(params.user, {
+        headers: {
+          Authorization: 'TOKEN be8dfaa7e2e1f95d35b56e70ad07c101d44f2dab',
+        },
+      })
+      .then((response) => {
+        const {
+          login,
+          name,
+          bio,
+          email,
+          avatar_url: avatarUrl,
+        } = response.data;
+        setUser({ login, name, bio, email, avatarUrl });
+      });
+
+    if (!!user) {
+      api
+        .get(`${params.user}/followers`, {
+          headers: {
+            Authorization: 'TOKEN be8dfaa7e2e1f95d35b56e70ad07c101d44f2dab',
+          },
+        })
+        .then((response) => setFolloweres(response.data.length));
+
+      api
+        .get(`${params.user}/following`, {
+          headers: {
+            Authorization: 'TOKEN be8dfaa7e2e1f95d35b56e70ad07c101d44f2dab',
+          },
+        })
+        .then((response) => setFollowing(response.data.length));
+    }
+  }, [params.user, user]);
+
   return (
     <>
       <Header />
-      <UserInfo>
-        <header>
-          <img
-            src="https://avatars0.githubusercontent.com/u/48018647?v=4"
-            alt="user"
-          />
-          <div>
-            <strong>Gabriel Nascimento</strong>
-            <p>Front End Developer</p>
+      {!!user ? (
+        <UserInfo>
+          <header>
+            <img src={user.avatarUrl} alt="user" />
+            <div>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </div>
+          </header>
+          <div className="infos">
+            <Info>
+              <p>Email</p>
+              <strong>{!!user.email ? user.email : 'Email privado'}</strong>
+            </Info>
+            <Info>
+              <p>Followers</p>
+              <strong>{followers}</strong>
+            </Info>
+            <Info>
+              <p>Following</p>
+              <strong>{following}</strong>
+            </Info>
           </div>
-        </header>
-        <div className="infos">
-          <Info>
-            <p>Email</p>
-            <strong>gabriel.nbds@gmail.com</strong>
-          </Info>
-          <Info>
-            <p>Followers</p>
-            <strong>11</strong>
-          </Info>
-          <Info>
-            <p>Following</p>
-            <strong>8</strong>
-          </Info>
-        </div>
-      </UserInfo>
+        </UserInfo>
+      ) : null}
     </>
   );
 };
